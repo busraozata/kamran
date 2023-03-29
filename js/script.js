272b512f62bd352557bfd380ace4233cc9b2f478
Büşra Özata <busraozata1998@gmail.com>
2023-03-29 18:30:01 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -1,5 +1,15 @@
+const menuToggle = document.querySelector("#toggle-btn");
+const navigation = document.querySelector(".navigation");
+menuToggle.onclick = function () {
+  menuToggle.classList.toggle("active");
+  navigation.classList.toggle("active");
 
-
+  if (menuToggle.innerHTML === `<i class="fa-solid fa-xmark"></i>`) {
+    menuToggle.innerHTML = `<i class="fa-solid fa-bars"></i>`;
+  } else {
+    menuToggle.innerHTML = `<i class="fa-solid fa-xmark"></i>`;
+  }
+};
 $(document).ready(function () {
   $(".services_toggle").click(function () {
     $(".services-menu").animate({ width: "toggle" });
@@ -18,65 +28,193 @@ var swiperTop = new Swiper(".mySwiperBanner", {
 });
 var swiperServices = new Swiper(".mySwiperServices", {
   spaceBetween: 0,
-  slidesPerView: "3",
   touchRatio: 0.2,
   loop: true,
   loopedSlides: 3,
-  autoplay: {
+  slidesPerView: 1,
+  /*  autoplay: {
     delay: 2500,
     disableOnInteraction: false,
+  }, */
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 0,
+    },
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 0,
+    },
+    1440: {
+      slidesPerView: 3,
+      spaceBetween: 00,
+    },
+    1650: {
+      slidesPerView: 3,
+      spaceBetween: 00,
+    },
   },
 });
 swiperTop.controller.control = swiperServices;
 swiperServices.controller.control = swiperTop;
 
 var swiper = new Swiper(".mySwiperComments", {
-  slidesPerView: 4,
-  spaceBetween: 30,
+  spaceBetween: 60,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
- /*  autoplay: {
+  /*  autoplay: {
     delay: 2500,
     disableOnInteraction: false,
   }, */
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+    },
+    1024: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+    },
+    1440: {
+      slidesPerView: 1,
+      spaceBetween: 00,
+    },
+    1650: {
+      slidesPerView: 2,
+      spaceBetween: 60,
+    },
+  },
 });
 var swiper = new Swiper(".mySwiperBlog", {
-  slidesPerView: 3,
-  spaceBetween: 0,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
+  },
+  mousewheel: {
+    eventsTarged: ".swiper-slide",
+    sensitivity: 5,
   },
   navigation: {
     nextEl: ".swiper-button-next",
     prevEl: ".swiper-button-prev",
   },
-/*   autoplay: {
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 15,
+    },
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 15,
+    },
+    1440: {
+      slidesPerView: 2,
+      spaceBetween: 15,
+    },
+    1650: {
+      slidesPerView: 2,
+      spaceBetween: 15,
+    },
+  },
+  /*   autoplay: {
     delay: 2500,
     disableOnInteraction: false,
   }, */
 });
 
-let SwiperTopVideo = new Swiper(".swiper--top", {
-  spaceBetween: 10,
-/*   centeredSlides: true, */
- /*  speed: 8000,
-  autoplay: {
-    delay: 1,
-  }, */
- 
-  slidesPerView: "2",
-/*   allowTouchMove: false,
-  disableOnInteraction: true, */
-
+var swiper = new Swiper(".mySwiperLastBlog", {
+  slidesPerView: 3,
+  spaceBetween: 70,
   pagination: {
     el: ".swiper-pagination",
     clickable: true,
   },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+    },
+    1024: {
+      slidesPerView: 2,
+      spaceBetween: 50,
+    },
+    1440: {
+      slidesPerView: 3,
+      spaceBetween: 70,
+    },
+  },
 });
 
+let SwiperTopVideo = new Swiper(".swiper--top", {
+  spaceBetween: 0,
+  slidesPerView: 1,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+    },
+    768: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+    },
+    1024: {
+      slidesPerView: 1,
+      spaceBetween: 0,
+    },
+    1440: {
+      slidesPerView: 2,
+      spaceBetween: 00,
+    },
+    1650: {
+      slidesPerView: 2,
+      spaceBetween: 00,
+    },
+  },
+});
+var swiper = new Swiper(".mySwiperSSS", {
+  slidesPerView: 1,
+  spaceBetween: 0,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  breakpoints: {
+    640: {
+      slidesPerView: 1,
+      spaceBetween: 20,
+    },
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 0,
+    },
+  },
+  autoplay: {
+    delay: 2500,
+    disableOnInteraction: false,
+  },
+});
 /* 
 const registerVideo = (bound, video) => {
 	bound = document.querySelector(bound);
@@ -96,8 +234,5 @@ const registerVideo = (bound, video) => {
 
 
 registerVideo(".services", ".services video"); */
-
-
-
 
 AOS.init();
